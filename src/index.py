49a9d6f4594b066f3a30ca7e6bf91d6542335d72
Eyo Chen <eyo.chen@amazingtalker.com>
2025-05-24 20:21:36 +0800

    temp

--- src/index.py
+++ src/index.py
@@ -1,21 +1,24 @@
 import grpc
-from concurrent import futures
-import proto.stock_pb2 as stock_pb2
 import proto.stock_pb2_grpc as stock_pb2_grpc
+from concurrent import futures
+from pymongo import MongoClient
 from dotenv import load_dotenv
+from handler.stock import StockService
+from adapters.repositories.stock.stock import StockRepository
+from usecase.stock.stock import StockUsecase
+
 
 load_dotenv()
 
 
-class StockService(stock_pb2_grpc.StockService):
-    def Create(self, request, context):
-        return stock_pb2.CreateResp(id="123")
-
-
 def serve():
+    client = MongoClient("mongodb://localhost:27017")
+    stock_repo = StockRepository(client, "stock_db")
+    stock_usecase = StockUsecase(stock_repo)
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    stock_pb2_grpc.add_StockServiceServicer_to_server(StockService(), server)
+    stock_pb2_grpc.add_StockServiceServicer_to_server(StockService(stock_usecase), server)
     server.add_insecure_port("[::]:50051")
+    print("server is running...")
     server.start()
     server.wait_for_termination()
 
