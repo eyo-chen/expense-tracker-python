cba0ffaeb188049632dca9d77827b3f6662064ab
YI YOU CHEN (Eyo) <zxcvbn1239104@gmail.com>
2025-05-31 09:15:30 +0800

    Feat: list stock usecase (#11)
    * refactor: file structure
    
    * feat: add list stock usecase
    
    * test: add unit testing
    
    ---------
    
    Co-authored-by: Eyo Chen <eyo.chen@amazingtalker.com>

--- src/usecase/base.py
+++ src/usecase/base.py
@@ -1,8 +1,13 @@
+from typing import List
 from abc import ABC, abstractmethod
-from domain.stock import CreateStock
+
+from domain.stock import CreateStock, Stock
 
 
 class AbstractStockUsecase(ABC):
     @abstractmethod
     def create(self, stock: CreateStock) -> str:
         """Create a new stock entry."""
+
+    def list(self, user_id: int) -> List[Stock]:
+        """List all stock by user id"""
--- src/usecase/stock.py
+++ src/usecase/stock.py
@@ -1,4 +1,6 @@
-from domain.stock import CreateStock
+from typing import List
+
+from domain.stock import CreateStock, Stock
 from adapters.base import AbstractStockRepository
 from .base import AbstractStockUsecase
 
@@ -9,3 +11,6 @@ class StockUsecase(AbstractStockUsecase):
 
     def create(self, stock: CreateStock):
         return self.stock_repo.create(stock)
+
+    def list(self, user_id: int) -> List[Stock]:
+        return self.stock_repo.list(user_id)
